042085c086e6b9d8bc23f9f57e9ca13c891a4c7e
Stooged <stooged@users.noreply.github.com>
2018-03-10 00:36:22 +0800

    r3

--- AppToUsb/source/main.c
+++ AppToUsb/source/main.c
@@ -358,13 +358,13 @@ void checkusbpkg(char *sourcedir, char* destdir) {
             {}
             else
             {
-            sprintf(upkg_path, "/%s/%s", usb_mount_path, dp->d_name);
+            sprintf(upkg_path, "%s/%s", usb_mount_path, dp->d_name);
             if (!stat(upkg_path, &info))
             {
-		if (file_compare(sourcedir, upkg_path))
-		{
                 if (S_ISREG(info.st_mode))
                 {
+		if (file_compare(sourcedir, upkg_path))
+		{
 		    rename(upkg_path, destdir);
 		    break;
 	        }
--- AppToUsb/source/patch.c
+++ AppToUsb/source/patch.c
@@ -71,26 +71,10 @@ int patcher(struct thread *td){
 	uint64_t cr0 = readCr0();
 	writeCr0(cr0 & ~X86_CR0_WP);
 
-	// specters debug settings patchs
-	*(char *)(kernel_base + 0x2001516) |= 0x14;
-	*(char *)(kernel_base + 0x2001539) |= 3;
-	*(char *)(kernel_base + 0x200153A) |= 1;
-	*(char *)(kernel_base + 0x2001558) |= 1;	
-
-	// debug menu full patches thanks to sealab
-	*(uint32_t *)(kernel_base + 0x4CECB7) = 0;
-	*(uint32_t *)(kernel_base + 0x4CFB9B) = 0;
-
-	// Target ID Patches :)
-	*(uint16_t *)(kernel_base + 0x1FE59E4) = 0x8101;
-	*(uint16_t *)(kernel_base + 0X1FE5A2C) = 0x8101;
-	*(uint16_t *)(kernel_base + 0x200151C) = 0x8101;
-
-	// enable mmap of all SELF ???
-	*(uint8_t*)(kernel_base + 0x31EE40) = 0x90;
-	*(uint8_t*)(kernel_base + 0x31EE41) = 0xE9;
-	*(uint8_t*)(kernel_base + 0x31EF98) = 0x90;
-	*(uint8_t*)(kernel_base + 0x31EF99) = 0x90;
+	//uart
+	//uint16_t *securityFlags = (uint64_t *)(kernel_base+0x2001516);
+	//*securityFlags = *securityFlags & ~(1 << 15);
+	//*(char *)(kernel_base + 0x186b0a0) = 0;
 
 	// Restore write protection
 	writeCr0(cr0);
